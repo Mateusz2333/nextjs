7f23cb28344bc6036605f3c3f42be73183890dd8
Mateusz2333 <mateusz.nowak1@microsoft.wsei.edu.pl>
2024-12-18 10:58:51 +0100

    6.0

--- tests/navigation.spec.js
+++ tests/navigation.spec.js
@@ -1,15 +1,11 @@
 const { test, expect } = require('@playwright/test');
 
 test('has link to login page', async ({ page }) => {
-  // Otwórz stronę główną
   await page.goto('http://localhost:3000/');
-
-  // Kliknij w link z tekstem "Sign In"
+ 
   await page.getByText('Sign In').click();
 
-  // Sprawdź, czy otworzyła się strona logowania
   await expect(page).toHaveURL('http://localhost:3000/public/user/signin');
 
-  // Sprawdź, czy na stronie logowania jest nagłówek z tekstem "Login"
   await expect(page.locator('h2')).toContainText('Login');
 });
--- tests/testlogowania.spec.js
+++ tests/testlogowania.spec.js
@@ -1,24 +1,18 @@
 const { test, expect } = require('@playwright/test');
 
 test('should login and navigate to user profile page', async ({ page }) => {
-  // Otwórz stronę logowania
+  
   await page.goto('http://localhost:3000/public/user/signin');
 
-  // Wypełnij formularz logowania
   await page.fill('#email', 'mateusz.nowak1@Microsoft.wsei.edu.pl');
   await page.fill('#password', 'password1');
-
-  // Zatwierdź formularz
+  
   await page.click('button[type="submit"]');
-
-  // Poczekaj, aż strona przeładuje się po zalogowaniu
+  
   await page.waitForLoadState('load');
-
-  // Teraz po zalogowaniu sprawdzimy, czy użytkownik ma dostęp do strony swojego profilu
-  // Spróbuj przejść na stronę profilu użytkownika
+  
   await page.goto('http://localhost:3000/protected/user/profile');
 
-  // Sprawdź, czy po zalogowaniu użytkownik ma dostęp do strony profilu
   await expect(page).toHaveURL('http://localhost:3000/protected/user/profile');
 });
  
