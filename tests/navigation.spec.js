a7bd812e797dad9cad52148d77ec769409c574db
Mateusz2333 <mateusz.nowak1@microsoft.wsei.edu.pl>
2024-12-18 11:01:03 +0100

    lab 10 DONE 6.0

--- tests/navigation.spec.js
+++ tests/navigation.spec.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@playwright/test');
 
 test('has link to login page', async ({ page }) => {
   await page.goto('http://localhost:3000/');
- 
+
   await page.getByText('Sign In').click();
 
   await expect(page).toHaveURL('http://localhost:3000/public/user/signin');
